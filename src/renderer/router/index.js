11bfbbb174c4eb2506cf3f2a7c752797446d5298
Ruurd Bijlsma <ruurd@bijlsma.dev>
2020-01-18 02:45:09 +0100

    Add /library to /library/playlist redirect to fix active status of tab, add Github screenshots

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -28,11 +28,7 @@ const routes = [
     },
     {
         path: '/library',
-        name: 'library',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/Library.vue')
+        redirect: '/library/playlists'
     },
     {
         path: '/library/*',
